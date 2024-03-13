48e96ce07f0530c02864aedc7899cfabbc3dbf88
halinchanok <116154769+halinchanok@users.noreply.github.com>
2024-03-13 23:17:23 +0700

    final

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ function showSlides(n) {
   }
 }
 
-// page2
+
+// page2 -----------------------------------------------------------------------------
 function changeImage() {
     const selectElement = document.getElementById("order_name");
     const selectedImage = document.getElementById("img_order_name");
@@ -28,15 +29,17 @@ function changeImage() {
 
 // เปิดปิดเเท็บจ่ายตัง
 let ch = 0;
-
 function openNav() {
   if(ch == 0){
     document.getElementById("cart22").style.width = "480px"; //เวลากดปุ่มจ่ายตังจะให้มันเลื่อนออกมาเท่าไหร่
-    document.getElementById("main").style.marginRight = "0px";
+    document.getElementById("main").style.marginLeft = "850px"; // ตำแหน่งที่เลื่อนเวลากด
     ch = 1;
   }else if (ch == 1){
     document.getElementById("cart22").style.width = "0px"; 
-    document.getElementById("main").style.marginRight = "0px";
+    document.getElementById("main").style.marginLeft = "1000px";
     ch = 0;
   }
 }
+
+
+// page3 ----------------------------------------------------------------------------
