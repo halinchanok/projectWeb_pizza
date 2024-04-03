49fde2b0a32fe2427e4783c7392bc135a7a57b22
a0ms1nn <162542840+a0ms1nn@users.noreply.github.com>
2024-04-03 16:07:53 +0700

    updatebest

--- page1_menu.php
+++ page1_menu.php
@@ -11,6 +11,8 @@
         <IMG class="HEAD" src="images/menu.png">
 
         <div class="container">
+            <img class="best" src="images/bestsell.png">
+            <img class="best2" src="images/bestsell.png">
             <div class="menu1">
                 <img src="images/cheese pizza.webp">
                 <div class="cs">CHEESE PIZZA</div>
@@ -46,8 +48,9 @@
         </div>
 
         <br>
-
+            
         <div class="container2">
+            <img class="best3" src="images/bestsell.png">
             <div class="menu">
                 <img src="images/MEAT DELUXE.webp">
                 <div class="cs">MEAT DELUXE</div> 
