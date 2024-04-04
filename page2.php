a9b7e3a54d54979781de818b820acf0fc32224f1
a0ms1nn <162542840+a0ms1nn@users.noreply.github.com>
2024-04-05 01:37:44 +0700

    up

--- page2.php
+++ page2.php
@@ -174,7 +174,7 @@
                     <button class="SUBMIT" type="submit" onclick="page3.html">submit</button>
                     </div>
                     <div id="main">
-                        <button class="openbtn" type="submit" onclick="openNav()" methode="ch">&#9776; จ่ายตัง</button>
+                        <button class="openbtn" type="submit" onclick="openNav()" methode="ch">&#9776; ORDER SUMMARY</button>
                     </div>
         
             </form>
@@ -221,6 +221,7 @@
                     }
                     ?>
                     <br>
+                    <br>
                     <p class='left'>-----------------------------------------------------</p><br>
                 <?php
                     if(isset($_SESSION['menu2']) && isset($_SESSION['size']) && isset($_SESSION['crust']) && isset($_SESSION['topping'])) {
@@ -233,6 +234,21 @@
                         echo "<div class='right'>฿0.00</div>";
                     }
                 ?>
+                <button type="button" class="button" id="top3">
+                <script>
+                    document.getElementById('top3').addEventListener('click', function() {
+                     window.location.href = 'page3.php'; // เปลี่ยนเส้นทางไปยังหน้าที่สอง
+                    });
+                </script>
+                    <svg viewBox="0 0 16 16" class="bi bi-coin" fill="currentColor" height="16" width="16" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"></path>
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"></path>
+                    <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path>
+                    </svg>
+                        PAYMENT
+                </button>
+                
+
             </div>
             
         </div>
---
+++ page3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="th">
+    <head>
+        <meta charset="UTF-8">
+        <title>page3</title>
+        <link rel="stylesheet" href="css/style-page3.css"> <!-- CSS -->
+    </head>
+
+    <body>
+        <script src="script.js"></script>
+        <img class="payment" src="images/payment.png">
+
+        <div class="prompay">
+
+        </div>
+    </body>
+</html>
+<!DOCTYPE html>
