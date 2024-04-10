b1d1ed973aa9901bfaf63eeee34c25f8713c6295
a0ms1nn <162542840+a0ms1nn@users.noreply.github.com>
2024-04-10 21:17:55 +0700

    up

--- page7.php
+++ page7.php
@@ -4,15 +4,15 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>page5</title>
-        <link rel="stylesheet" href="css/style-page5.css"> <!-- CSS -->
+        <link rel="stylesheet" href="css/style-page6.css"> <!-- CSS -->
     </head>
 
     <body>
-            <img class="success" src="images/success.png">
+            <img class="success" src="images/finish.png">
             <script>
                 document.querySelector('.success').addEventListener('click', function() {
                     // นำผู้ใช้ไปยังหน้าถัดไป
-                window.location.href = 'page6.php';
+                window.location.href = 'index.php';
                  });
             </script>
     </body>
